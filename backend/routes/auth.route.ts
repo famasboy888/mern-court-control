83b4065b63e85793cffa40403ec8396d86e37fcd
judd.gnizama <juddmaverick@gmail.com>
2024-09-01 01:57:57 +0800

    fixes #11 - added rateLimiter

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controller.js";
 import { validateRegisterInput } from "../middlewares/validation.middleware.js";
+import rateLimit from "express-rate-limit";
 
 const router = Router();
 
-router.post("/register", validateRegisterInput, registerUser);
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 20,
+  message: { msg: "IP rate limit exceeded. Retry after 15 minutes" },
+});
+
+router.post("/register", apiLimiter, validateRegisterInput, registerUser);
 
 export { router as authRouter };
